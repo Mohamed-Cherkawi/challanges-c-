3f80263a06ce5ab517241232c6542f2cbf864f8d
Mohamed Cherkaoui <emailforstudy911@gmail.com>
2021-11-12 09:30:10 +0100

    changed folder name

---
+++ Quiz C variables/quiz 13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int num;
+	
+	printf("Enter a random number : \n");
+		scanf("%d" , &num);
+	printf(" The hexadécimale form of this number is : %p");
+	
+	
+	
+	return 0;
+}
