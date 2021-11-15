1af30ee9ea4d5d90b872977df71f20bb6787bc1a
Mohamed Cherkaoui <emailforstudy911@gmail.com>
2021-11-15 10:12:12 +0100

    Added Quiz 9

---
+++ QUiz-C-Conditions/Quiz 9.c
@@ -0,0 +1,19 @@
+int main(){
+	float num1, num2;
+	printf("Enter num 1 : \n");
+		scanf("%f",&num1);
+			printf("Enter num 2 : \n");
+				scanf("%f",&num2);
+	
+	if(num1 > num2){
+		printf("Num1 is the biggest : %f > %f",num1,num2);
+		
+	} else {
+		printf("Num2 is the biggest : %f > %f",num2,num1);
+	}
+		
+	
+	
+	
+	return 0;
+}
